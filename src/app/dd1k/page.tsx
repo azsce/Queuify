9f5ed76d86c22e6dfd6215828e4f44547a602119
Ahmed Hosny <ahhosnyas@gmail.com>
2024-12-09 20:35:37 +0000

    Update metadata for DD1KExplanation page and RootLayout to reflect new titles

--- src/app/dd1k/page.tsx
+++ src/app/dd1k/page.tsx
@@ -1,5 +1,10 @@
 import DD1KExplanation from "@/views/DD1KExplanation";
 import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "D/D/1/K Queue Model",
+};
 
 const DD1KExplanationPage: React.FC = () => {
   return <DD1KExplanation />;
