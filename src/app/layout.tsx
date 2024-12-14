b1a230b0058eedb3f787bdd8ee4ac0e7c030887f
ahmedhosnypro <ahhosnyas@gmail.com>
2024-12-14 01:25:15 +0000

    G-tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata, Viewport } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Box } from "@mui/material";
 import TopAppBar from "@/components/base/TopAppBar";
@@ -8,29 +7,62 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import MuiThemeProvider from "@/components/base/MuiThemeProvider";
 import { DD1KProvider } from "@/contexts/DD1KContext";
 import { MMProvider } from "@/contexts/MMContext";
+import { Geist, Geist_Mono } from "next/font/google";
+import Head from "next/head";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const geistSans = Geist({
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
 });
+export const metadata: Metadata = {
+  title: "Queuify - Queuing Theory Calculator and Simulator",
+  description:
+    "Queuify is an interactive and easy-to-use queuing theory calculator and simulator. Perform M/M/S/K and D/D/1/K queue analyses with precision and real-time visualizations.",
+  icons: {
+    icon: "/icon-192x192.png",
+    apple: "/icon-192x192.png",
+  },
+  keywords: [
+    "Queuify",
+    "queuing theory",
+    "queuing calculator",
+    "queue simulator",
+    "M/M/S/K queue",
+    "M/M/1 queue",
+    "M/M/1/K queue",
+    "M/M/C queue",
+    "M/M/C/K queue",
+    "D/D/1/K queue",
+    "D/D/1/K-1 queue",
+    "queuing analysis",
+  ],
+  authors: [{ name: "Ahmed Hosny", url: "https://github.com/ahmedhosnypro" }],
+  openGraph: {
+    title: "Queuify - Queuing Theory Calculator and Simulator",
+    description:
+      "Perform precise calculations and simulations for queuing models such as M/M/1 and D/D/1/K. Experience an intuitive interface with real-time results.",
+    url: "https://azsce.github.io/Queuify",
+    siteName: "Queuify",
+    type: "website",
+    images: [
+      {
+        url: "/icon-512x512.png",
+        width: 1200,
+        height: 630,
+        alt: "Queuify - Queuing Theory Calculator and Simulator",
+      },
+    ],
+  },
+};
 
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
-};
-
-export const metadata: Metadata = {
-  title: "Queuify",
-  icons: {
-    apple: "/icon-192x192.png",
-  },
+  userScalable: true,
 };
 
 export default function RootLayout({
@@ -40,8 +72,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="color-scheme" content="dark light" />
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebApplication",
+              name: "Queuify",
+              url: "https://azsce.github.io/Queuify",
+              description:
+                "An interactive calculator and simulator for queuing theory models like M/M/1 and D/D/1/K.",
+              applicationCategory: "Calculator",
+              operatingSystem: "Web",
+              author: {
+                "@type": "Person",
+                name: "Ahmed Hosny",
+                url: "https://github.com/ahmedhosnypro",
+              },
+            }),
+          }}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-D4KKXZ1SH9');
+            `,
+          }}
+        />
+      </Head>
+      ;
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.style.fontFamily} ${geistMono.style.fontFamily} antialiased`}
       >
         <ThemeProvider
           attribute="class"
@@ -67,6 +135,7 @@ export default function RootLayout({
           </AppRouterCacheProvider>
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId="G-D4KKXZ1SH9" />
     </html>
   );
 }
