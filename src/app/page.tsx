4877a56a07f2df964600601491c2d109c919fde3
ahmedhosnypro <ahhosnyas@gmail.com>
2024-12-14 01:42:25 +0000

    gh-pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
+import { Typography } from "@mui/material";
 
 export default function Home() {
   useEffect(() => {
     redirect("/calc");
   });
 
-  return <></>
+  return <Typography variant="subtitle1">...Loading</Typography>;
 }
